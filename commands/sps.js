0622ceae452dcfc0ad2fd17cb3153d02cbaa45ac
suptower <70473844+arda-kocer@users.noreply.github.com>
2021-03-19 18:54:21 +0100

    next fix

--- commands/sps.js
+++ commands/sps.js
@@ -5,7 +5,7 @@ module.exports = {
 	execute(message, args) {
         console.log("sps command has been initiated.");
         if (message.guild.available) {
-            let users = message.guild.members.array();
+            let users = message.guild.members.cache.array();
             let activeUsers= [];
             for (let i = 0; i < users.length; i++) {
                 if (users[i].presence.status!="offline") {

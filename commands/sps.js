025d69b392ca4819a6f84fe87d868f3c082a3793
suptower <70473844+arda-kocer@users.noreply.github.com>
2021-03-19 19:03:57 +0100

    amk es fängt wieder an

--- commands/sps.js
+++ commands/sps.js
@@ -8,7 +8,7 @@ module.exports = {
             let users = Array.from(message.guild.members.cache);
             let activeUsers= [];
             for (let i = 0; i < users.length; i++) {
-                if (users[i].presence.status!="offline") {
+                if (users[i].user.presence.status!="offline") {
                     activeUsers.push(users[i]);
                 }
             }

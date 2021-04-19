c7ca271f1a7e039d168257c403326502ef8fe93a
suptower <70473844+arda-kocer@users.noreply.github.com>
2021-04-19 13:13:58 +0200

    yooli update
    HEUT IST EIN GUTER TAG

---
+++ commands/yooli.js
@@ -0,0 +1,84 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'yooli',
+    description: 'yooli',
+    cooldown: 10,
+	execute(message, args) {
+        let stream = "";
+        const auRole = message.guild.roles.cache.find(role => role.name === "AUSOUP"); 
+        console.log("yooli command has been initiated.");
+        var audios = ['./yooli/aktien.mp3','./yooli/designer.mp3','./yooli/feature.mp3','./yooli/flow.mp3','./yooli/gurl.mp3','./yooli/gutertag.mp3','./yooli/monat12k.mp3',
+        './yooli/neuestelefon.mp3','./yooli/oma.mp3','./yooli/sandwichnight.mp3','./yooli/schamhaar.mp3'];
+        let value = Math.round(Math.random()*(audios.length-1));
+        stream = audios[value];
+    if (!args.length && message.member.voice.channel) {
+        const voiceChannel = message.member.voice.channel;
+
+        voiceChannel.join().then(connection => {
+            const dispatcher = connection.play(stream);
+            dispatcher.on('finish',() => voiceChannel.leave());
+    });
+    }
+    else if (!args.length) {
+        return message.reply('you need to be connected to a voice channel to use this command.');
+    }
+    else if (!(isNaN(parseInt(args[0])))) {
+        if (parseInt(args[0])>=0 && parseInt(args[0]) < audios.length) {
+            stream = audios[parseInt(args[0])];
+            if (!args[1] && message.member.voice.channel) {
+                const voiceChannel = message.member.voice.channel;
+
+                voiceChannel.join().then(connection => {
+                    const dispatcher = connection.play(stream);
+                    dispatcher.on('finish',() => voiceChannel.leave());
+                })
+            }
+            else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+                if (!auRole) {
+                    return message.reply("this server has not configured 'AUSOUP'.\nPlease create this role to configure permissions for alternative usage of commands.");
+                }
+                else {
+                    if (!message.member.roles.cache.has(auRole.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                        return message.reply("you are not allowed to use this command.\nYou need to have 'AUSOUP' role assigned to execute.");
+                    }
+                    else {
+                        const voiceChannel = message.mentions.members.first().voice.channel;
+                        voiceChannel.join().then(connection => {
+                        const dispatcher = connection.play(stream);
+                        dispatcher.on('finish',() => voiceChannel.leave());
+                        })
+                    }
+                    
+                }
+            }
+            else {
+                message.reply('No connection available.');
+            }
+        }
+    }
+    else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+        if (!auRole) {
+            return message.reply("this server has not configured 'AUSOUP'.\nPlease create this role to configure permissions for alternative usage of commands.");
+        }
+        else {
+            if (!message.member.roles.cache.has(auRole.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                return message.reply("you are not allowed to use this command.\nYou need to have 'AUSOUP' role assigned to execute.");
+            }
+            else {
+                const voiceChannel = message.mentions.members.first().voice.channel;
+                voiceChannel.join().then(connection => {
+                const dispatcher = connection.play(stream);
+                dispatcher.on('finish',() => voiceChannel.leave());
+                })
+            }
+            
+        }
+    }
+    else if (args[0] === "info") {
+        message.channel.send("```0 - Einfach warten\n1 - Coppe Designer\n2 - Willst du Feature?\n3 - Flowmonster\n4 - GUUUUUUUUUUUUUUUUUURL\n5 - GUTER TAG\n6 - IM MOONAT 12K\n7 - NEUES TELEFON\n8 - EINE OMA EINE OMA\n9 - SANDWICH NIGHT\n10 - SCHAMHAAR```");
+    }
+    else {
+        message.reply(`this user is currently not connected to any voice channel.`);
+        }
+    },
+};

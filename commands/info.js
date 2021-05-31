be81afc2bc11243fa982ae6ba85e8cea293668d4
suptower <ardakocer@live.de>
2021-05-31 18:38:50 +0200

    ntm update
    NICE TO MEET YA

--- commands/info.js
+++ commands/info.js
@@ -16,7 +16,7 @@ module.exports = {
                 { name: 'Documentation (not up to date)', value: 'http://bit.ly/soupbotDoc' },
             )
             .setTimestamp()
-            .setFooter('Version 1.1.91 | Updated: 24th of May, 2021');
+            .setFooter('Version 1.1.92 | Updated: 31st of May, 2021');
 
             message.channel.send(InfoEmbed);
 	},
